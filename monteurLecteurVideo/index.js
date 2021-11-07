ff049f434a28a754f15861f1a8e44cf54dec363f
MorganeU <72866789+MorganeU@users.noreply.github.com>
2021-11-07 22:23:23 +0100

    ajouter video + tests d'une nouvelle fct

--- monteurLecteurVideo/index.js
+++ monteurLecteurVideo/index.js
@@ -10,83 +10,96 @@ template.innerHTML = /*html*/`
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />    <script src="./monteurLecteurVideo/index.js" type="module"></script>
 
 <style>
-#player {
-  height: 500px;
-}
-#barre {
-  background-color:rgba(0, 0, 0, .5);
-  position:absolute;
-  height:35px;
-  bottom: 0;
-  opacity:0;
-  display: flex;
-  align-items: center;
-  transition: opacity cubic-bezier(0.4, 0, 1, 1) .3s;
-}
-#barre > button {
-  display: block;
-}
-#over {
-  width: fit-content;
-  position: relative;
-  margin: 25px;
-  color:white;
-  font-family: Arial;
-}
-#over:hover > #barre {
-  opacity:1;
-}
-#sec{
-  margin-left:auto;
-}
-#ecran {
-  margin-right: 5px;
-}
-#infos {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-}
-i {
-  color:white;
-}
-button {
-  all:unset;
-  padding: 0 8px;
-  cursor: pointer;
-  color:white;
-  font-size:20px;
-}
-#play > i{
-  font-size:18px;
-  padding:0 1px;
-}
-#vol {
-  font-size:25px;
-  margin-left:5px;
-  width:30px;
-}
-#temps{
-  padding-left:20px;
-}
-#avancement{
-  position:absolute;
-  bottom:35px;
-  width:100%;
-  height:5px;
-  background-color:white;
-  opacity:0;
-  cursor: pointer;
-  transition: opacity cubic-bezier(0.4, 0, 1, 1) .3s;
-}
-#avancement > div{
-  height:5px;
-  width:0;
-  background-color:blue;
-}
-#over:hover > #avancement {
-  opacity:1;
-}
+  #player {
+    height: 500px;
+  }
+  #barre {
+    background-color:rgba(0, 0, 0, .5);
+    position:absolute;
+    height:35px;
+    bottom: 0;
+    opacity:0;
+    display: flex;
+    align-items: center;
+    transition: opacity cubic-bezier(0.4, 0, 1, 1) .3s;
+  }
+  #barre > button {
+    display: block;
+  }
+  #over {
+    width: fit-content;
+    position: relative;
+    margin: 25px;
+    color:white;
+    font-family: Arial;
+  }
+  #over:hover > #barre,  #over:hover > #infos, #over:hover > #avancement {
+    opacity:1;
+  }
+  #infos{
+    opacity:0;
+    transition: opacity cubic-bezier(0.4, 0, 1, 1) .3s;
+  }
+  #sec{
+    margin-left:auto;
+    font-size:16px;
+  }
+  #vite{
+    font-size:16px;
+  }
+  #ecran {
+    margin-right: 5px;
+  }
+  #infos {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+  i {
+    color:white;
+  }
+  button {
+    all:unset;
+    padding: 0 8px;
+    cursor: pointer;
+    color:white;
+    font-size:20px;
+  }
+  #play > i{
+    font-size:18px;
+    padding:0 1px;
+  }
+  #vol {
+    font-size:25px;
+    margin-left:5px;
+    width:30px;
+  }
+  #temps{
+    padding-left:20px;
+  }
+  #avancement{
+    position:absolute;
+    bottom:35px;
+    width:100%;
+    height:5px;
+    background-color:white;
+    opacity:0;
+    cursor: pointer;
+    transition: opacity cubic-bezier(0.4, 0, 1, 1) .3s;
+  }
+  #avancement > div{
+    height:5px;
+    width:0;
+    background-color:#8FA6CB;
+  }
+  #liste{
+    height:200px;
+    width:200px;
+  }
+  #nvlleVideo{
+    margin: 25px;
+    font-family: Arial;
+  }
 </style>
 
 <div id="over">
@@ -96,7 +109,7 @@ button {
     <button id="play"><i class="fas fa-play"></i></button>
     <button id="suivante"><i class="fas fa-step-forward"></i></button>
     <i id="vol" class="fas fa-volume-up"></i>
-    <webaudio-slider id="volume" height=17 width=70 tracking="rel" min=0 max=1 value=0.85 step="0.1"></webaudio-slider>
+    <webaudio-slider id="volume" height=17 width=70 tracking="rel" min=0 max=1 value=0.85 step="0.05"></webaudio-slider>
     <div id="temps">0:00 / 0:00</div>
     <button id="sec">+10s</button>
     <button id="vite">x1</button>
@@ -107,6 +120,14 @@ button {
     <div></div>
   </div>
 </div>
+<div id="nvlleVideo">
+  <p>Ajouter le lien d'une vidéo (.mp4) : </p>
+  <form>
+    <input id="input" type="text"></input>
+    <input id="ajout" type="button" value="OK"></input>
+  </form>
+</div>
+<div id="liste"></div>
 `;
 // <webaudio-knob diameter=50 id="volume" min=0 max=1 value=0.5 step="0.1" tooltip="%s" src="./assets/SimpleFlat3.png"></webaudio-knob>
 // <webaudio-knob diameter=40 id="volume" min=0 max=1 value=0.5 step="0.1" tooltip="%s" src="./assets/SimpleFlat3.png"></webaudio-knob>
@@ -155,10 +176,12 @@ class MyVideoPlayer extends HTMLElement {
         icon.style.padding = '0 1px';
       }
     }
+
     // Avancer de 10 sec la vidéo
     this.shadowRoot.querySelector("#sec").onclick = () => {
       this.player.currentTime += 10;
     }
+
     // Mettre la vidéo en vitesse x4
     this.shadowRoot.querySelector("#vite").onclick = (e) => {
       if (this.player.playbackRate == 1) this.player.playbackRate = 2;
@@ -166,35 +189,43 @@ class MyVideoPlayer extends HTMLElement {
       else if (this.player.playbackRate == 4) this.player.playbackRate = 1;
       e.currentTarget.innerHTML = 'x' + this.player.playbackRate;
     }
+
     // Obtenir dans la console des informations sur la vidéo
     this.shadowRoot.querySelector("#infos").onclick = () => {
+      console.log("Lien de la video : " + this.player.src)
       console.log("Durée de la vidéo : " + this.player.duration);
       console.log("Temps courant : " + this.player.currentTime);
+      console.log("Volume de la video : " + this.player.volume);
+      console.log("Vitesse de la video : " + this.player.playbackRate);
     }
+
     // Mettre en grand écran la vidéo
     this.shadowRoot.querySelector("#ecran").onclick = () => {
       this.player.requestFullscreen();
     }
+
     // Changer de vidéo
-    let videos = ["https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
+    this.videos = [
       "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4",
-      "https://samplelib.com/lib/preview/mp4/sample-5s.mp4"
-    ];
+      "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+    ]; //"https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
     this.cpt = 0;
     this.shadowRoot.querySelector("#suivante").onclick = () => { // passer à la suivante
-      this.videoSuivante(videos)
+      this.videoSuivante()
     }
     this.shadowRoot.querySelector("#precedente").onclick = () => { // passer à la precedente
-      this.videoPrecedente(videos)
+      this.videoPrecedente()
     }
     this.player.onended = () => { // passer à la suivante quand la video est fini
-      this.videoSuivante(videos);
+      this.videoSuivante();
       this.player.play();
       let icon = this.shadowRoot.querySelector("#play > i");
       icon.className = 'fas fa-pause';
       icon.style.fontSize = 20 + 'px';
       icon.style.padding = '0px';
     }
+
     // Augmenter et baisser le volume
     this.shadowRoot.querySelector("#volume").oninput = (event) => {
       const vol = parseFloat(event.target.value);
@@ -206,6 +237,7 @@ class MyVideoPlayer extends HTMLElement {
       else iconName = 'fas fa-volume-up'
       icon.className = iconName
     }
+
     // Avancement vidéo
     this.player.ontimeupdate = () => {
       // affichage du temps
@@ -213,13 +245,22 @@ class MyVideoPlayer extends HTMLElement {
       // barre d'avancement
       this.shadowRoot.querySelector("#avancement > div").style.width = this.player.currentTime / this.player.duration * 100 + '%';
     };
-
     const barreAvancement = this.shadowRoot.querySelector("#avancement")
     barreAvancement.onclick = (e) => {
       const x = e.clientX - 30
       const width = barreAvancement.getBoundingClientRect().width
-      this.player.currentTime = this.player.duration * (x /width)
+      this.player.currentTime = this.player.duration * (x / width)
     }
+
+    // Ajouter une video à la liste
+    this.shadowRoot.querySelector("#ajout").onclick = () => {
+      this.videos.push(this.shadowRoot.querySelector("#input").value);
+      console.log(this.videos);
+    }
+
+    // Liste des autres videos (ne fonctionne pas)
+    // this.autresVideos();
+
   }
 
   fixRelativeURLS() {
@@ -231,10 +272,10 @@ class MyVideoPlayer extends HTMLElement {
     });
   }
 
-  videoSuivante(videos) {
+  videoSuivante() {
     this.cpt++;
-    if (this.cpt > videos.length - 1) { this.cpt = 0 }
-    this.player.src = videos[this.cpt];
+    if (this.cpt > this.videos.length - 1) { this.cpt = 0 }
+    this.player.src = this.videos[this.cpt];
     let icon = this.shadowRoot.querySelector("#play > i");
     icon.className = 'fas fa-play';
     icon.style.fontSize = 18 + 'px';
@@ -242,17 +283,17 @@ class MyVideoPlayer extends HTMLElement {
     this.shadowRoot.querySelector("#avancement > div").style.width = '0%';
   }
 
-  videoPrecedente(videos) {
+  videoPrecedente() {
     this.cpt--;
-    if (this.cpt < 0) { this.cpt = videos.length - 1 }
-    this.player.src = videos[this.cpt];
+    if (this.cpt < 0) { this.cpt = this.videos.length - 1 }
+    this.player.src = this.videos[this.cpt];
     let icon = this.shadowRoot.querySelector("#play > i");
     icon.className = 'fas fa-play';
     icon.style.fontSize = 18 + 'px';
     icon.style.padding = '0 1px';
     this.shadowRoot.querySelector("#avancement > div").style.width = '0%';
   }
-  
+
   dimensionVideo(v) {
     v.addEventListener("loadedmetadata", () => {
       const barre = this.shadowRoot.querySelector('#barre')
@@ -270,10 +311,20 @@ class MyVideoPlayer extends HTMLElement {
     if (seconds < 10) seconds = '0' + seconds;
     return minutes + ":" + seconds;
   }
+
+  autresVideos() {
+    for (let i = 0; i < this.videos.length; i++) {
+      if (this.player.src != this.videos[i]) {
+        this.shadowRoot.querySelector("#liste").innerHTML = '<video src=' + this.videos[i] + '></video>';
+        console.log(this.shadowRoot.querySelector("#liste").innerHTML)
+      }
+    }
+  }
 }
 
 customElements.define("my-player", MyVideoPlayer);
 
 
-// sources
+
+// Sources
 // http://g200kg.github.io/webaudio-controls/docs/knobgallery.html
